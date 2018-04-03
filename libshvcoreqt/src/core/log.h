7b1ea6706830b48bb3ecc71339a12538e6379389
Martin Špirk <spirk@elektroline.cz>
2018-04-03 14:04:52 +0200

    extented logging of qt types

--- libshvcoreqt/src/core/log.h
+++ libshvcoreqt/src/core/log.h
@@ -5,6 +5,10 @@
 
 #include <QString>
 #include <QByteArray>
+#include <QDateTime>
 
 inline NecroLog &operator<<(NecroLog log, const QString &s) { return log.operator <<(s.toUtf8().constData()); }
 NecroLog &operator<<(NecroLog log, const QByteArray &s);
+inline NecroLog &operator<<(NecroLog log, const QDate &d) { return log << d.toString(); }
+inline NecroLog &operator<<(NecroLog log, const QDateTime &d) { return log << d.toString(); }
+inline NecroLog &operator<<(NecroLog log, const QTime &d) {  return log << d.toString(); }
