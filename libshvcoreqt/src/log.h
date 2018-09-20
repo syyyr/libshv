6f45f4d6c19b994fadd4d4e7f386f4cfb16536a5
Martin Špirk <spirk@elektroline.cz>
2018-09-20 16:10:57 +0200

    improved logging of QT types

--- libshvcoreqt/src/log.h
+++ libshvcoreqt/src/log.h
@@ -7,8 +7,11 @@
 #include <QString>
 #include <QByteArray>
 #include <QDateTime>
+#include <QVariant>
 
 inline NecroLog &operator<<(NecroLog log, const QString &s) { return log.operator <<(s.toUtf8().constData()); }
+inline NecroLog &operator<<(NecroLog log, const QStringList &s) { return log.operator<<(("(" + s.join(", ") + ")").toStdString()); }
+inline NecroLog &operator<<(NecroLog log, const QVariant &v) { return log.operator<<(v.toString().toStdString()); }
 SHVCOREQT_DECL_EXPORT NecroLog &operator<<(NecroLog log, const QByteArray &s);
 inline NecroLog &operator<<(NecroLog log, const QDate &d) { return log << d.toString(Qt::ISODate); }
 inline NecroLog &operator<<(NecroLog log, const QDateTime &d) { return log << d.toString(Qt::ISODate); }
