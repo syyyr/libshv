11982fdb49f0c35f30cab1cefb007b955702a999
Fanda Vacek <vacek@elektroline.cz>
2018-12-18 18:10:53 +0100

    gitlab#12 TBus device and TBus Agent have the same deviceID

--- libshviotqt/src/rpc/deviceconnection.cpp
+++ libshviotqt/src/rpc/deviceconnection.cpp
@@ -2,6 +2,9 @@
 #include "../utils/fileshvjournal.h"
 
 #include <shv/core/log.h>
+#include <shv/core/string.h>
+
+#include <QCoreApplication>
 
 #include <fstream>
 
@@ -48,15 +51,23 @@ void DeviceConnection::setCliOptions(const DeviceAppCliOptions *cli_opts)
 		chainpack::RpcValue::Map opts = connectionOptions().toMap();
 		cp::RpcValue::Map dev;
 		std::string device_id = cli_opts->deviceId();
+		std::string device_id_file;
 		if(device_id.empty()) {
-			std::string device_id_file = cli_opts->deviceIdFile();
-			if(!device_id_file.empty()) {
-				std::ifstream ifs(device_id_file, std::ios::binary);
+			std::string fn = cli_opts->deviceIdFile();
+			if(!fn.empty()) {
+				std::ifstream ifs(fn, std::ios::binary);
 				if(ifs)
-					ifs >> device_id;
+					ifs >> device_id_file;
 				else
-					shvError() << "Cannot read device ID file:" << device_id_file;
+					shvError() << "Cannot read device ID file:" << fn;
 			}
+		}
+		if(device_id.empty()) {
+			device_id = device_id_file;
+		}
+		else {
+			shv::core::String::replace(device_id, "{{deviceIdFile}}", device_id_file);
+			shv::core::String::replace(device_id, "{{appName}}", QCoreApplication::applicationName().toStdString());
 		}
 		if(!device_id.empty())
 			dev[cp::Rpc::KEY_DEVICE_ID] = device_id;
