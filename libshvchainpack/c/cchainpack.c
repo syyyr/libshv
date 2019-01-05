3b9962218b3646177e8d9ecebcbe0c3ec6acf389
Fanda Vacek <vacek@elektroline.cz>
2019-01-05 21:47:48 +0100

    superfluous while removed

--- libshvchainpack/c/cchainpack.c
+++ libshvchainpack/c/cchainpack.c
@@ -444,27 +444,27 @@ static void unpack_uint(ccpcp_unpack_context* unpack_context, uint64_t *pval, in
 {
 	uint64_t num = 0;
 	int bitlen = 0;
-	do {
-		const char *p;
+
+	const char *p;
+	UNPACK_TAKE_BYTE();
+	uint8_t head = *p;
+
+	int bytes_to_read_cnt;
+	if     ((head & 128) == 0) {bytes_to_read_cnt = 0; num = head & 127; bitlen = 7;}
+	else if((head &  64) == 0) {bytes_to_read_cnt = 1; num = head & 63; bitlen = 6 + 8;}
+	else if((head &  32) == 0) {bytes_to_read_cnt = 2; num = head & 31; bitlen = 5 + 2*8;}
+	else if((head &  16) == 0) {bytes_to_read_cnt = 3; num = head & 15; bitlen = 4 + 3*8;}
+	else {
+		bytes_to_read_cnt = (head & 0xf) + 4;
+		bitlen = bytes_to_read_cnt * 8;
+	}
+
+	for (int i = 0; i < bytes_to_read_cnt; ++i) {
 		UNPACK_TAKE_BYTE();
-		uint8_t head = *p;
+		uint8_t r = *p;
+		num = (num << 8) + r;
+	};
 
-		int bytes_to_read_cnt;
-		if     ((head & 128) == 0) {bytes_to_read_cnt = 0; num = head & 127; bitlen = 7;}
-		else if((head &  64) == 0) {bytes_to_read_cnt = 1; num = head & 63; bitlen = 6 + 8;}
-		else if((head &  32) == 0) {bytes_to_read_cnt = 2; num = head & 31; bitlen = 5 + 2*8;}
-		else if((head &  16) == 0) {bytes_to_read_cnt = 3; num = head & 15; bitlen = 4 + 3*8;}
-		else {
-			bytes_to_read_cnt = (head & 0xf) + 4;
-			bitlen = bytes_to_read_cnt * 8;
-		}
-
-		for (int i = 0; i < bytes_to_read_cnt; ++i) {
-			UNPACK_TAKE_BYTE();
-			uint8_t r = *p;
-			num = (num << 8) + r;
-		};
-	} while(false);
 	if(pval)
 		*pval = num;
 	if(pbitlen)
