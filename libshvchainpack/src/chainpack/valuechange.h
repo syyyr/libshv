5f276f6e10fda96236488c704392ddba937bac5d
Fanda Vacek <fanda.vacek@gmail.com>
2020-02-19 18:26:01 +0100

    ValueChange getters and setters

--- libshvchainpack/src/chainpack/valuechange.h
+++ libshvchainpack/src/chainpack/valuechange.h
@@ -27,16 +27,19 @@ public:
 		static void registerMetaType();
 	};
 public:
-	//SHV_IMAP_FIELD_IMPL(shv::chainpack::RpcValue, MetaType::Key::Value, v, setV, alue)
-	//SHV_IMAP_FIELD_IMPL(shv::chainpack::RpcValue::DateTime, MetaType::Key::DateTime, d, setD, ateTime)
-	//SHV_IMAP_FIELD_IMPL(unsigned, MetaType::Key::ShortTime, s, setS, hortTime)
-public:
-	//using Super::Super;
 	ValueChange() {}
 	ValueChange(const RpcValue &val, unsigned short_time);
 	ValueChange(const RpcValue &val, const shv::chainpack::RpcValue::DateTime &date_time);
 	ValueChange(const RpcValue &val, const shv::chainpack::RpcValue::DateTime &date_time, unsigned short_time);
 	ValueChange(const RpcValue &o);
+
+	bool hasDateTime() const { return metaData().hasKey(MetaType::Tag::DateTime); }
+	shv::chainpack::RpcValue::DateTime dateTime() const { return metaValue(MetaType::Tag::DateTime).toDateTime(); }
+	void setDateTime(const shv::chainpack::RpcValue::DateTime &dt) { setMetaValue(MetaType::Tag::DateTime, dt); }
+
+	bool hasShortTime() const { return metaData().hasKey(MetaType::Tag::ShortTime); }
+	unsigned shortTime() const { return metaValue(MetaType::Tag::ShortTime).toUInt(); }
+	void setShortTime(unsigned t) { setMetaValue(MetaType::Tag::ShortTime, t); }
 };
 
 } // namespace chainpack
