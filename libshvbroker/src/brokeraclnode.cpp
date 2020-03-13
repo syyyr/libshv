7691632edcb2de2cca8d977f86856327bf611eda
Fanda Vacek <fanda.vacek@gmail.com>
2020-03-13 21:15:47 +0100

    SET medhod removed from ACL subnodes

--- libshvbroker/src/brokeraclnode.cpp
+++ libshvbroker/src/brokeraclnode.cpp
@@ -28,7 +28,7 @@ static std::vector<cp::MetaMethod> meta_methods_property {
 	{cp::Rpc::METH_DIR, cp::MetaMethod::Signature::RetParam, cp::MetaMethod::Flag::None, cp::Rpc::ROLE_READ},
 	{cp::Rpc::METH_LS, cp::MetaMethod::Signature::RetParam, cp::MetaMethod::Flag::None, cp::Rpc::ROLE_READ},
 	{cp::Rpc::METH_GET, cp::MetaMethod::Signature::RetVoid, cp::MetaMethod::Flag::IsGetter, cp::Rpc::ROLE_READ},
-	{cp::Rpc::METH_SET, cp::MetaMethod::Signature::RetVoid, cp::MetaMethod::Flag::IsSetter, cp::Rpc::ROLE_CONFIG},
+	//{cp::Rpc::METH_SET, cp::MetaMethod::Signature::RetVoid, cp::MetaMethod::Flag::IsSetter, cp::Rpc::ROLE_CONFIG},
 };
 
 static const std::string M_VALUE = "value";
