04ec5cda2bd04919fd6bf80074f42b22badee456
Fanda Vacek <fanda.vacek@gmail.com>
2020-11-18 18:27:30 +0100

    ShvLogRpcValueReader can throw

--- libshvcore/src/utils/shvlogrpcvaluereader.h
+++ libshvcore/src/utils/shvlogrpcvaluereader.h
@@ -13,7 +13,7 @@ class ShvJournalEntry;
 class SHVCORE_DECL_EXPORT ShvLogRpcValueReader
 {
 public:
-	ShvLogRpcValueReader(const shv::chainpack::RpcValue &log);
+	ShvLogRpcValueReader(const shv::chainpack::RpcValue &log, bool throw_exceptions = false);
 
 	bool next();
 	const ShvJournalEntry& entry() { return m_currentEntry; }
@@ -24,6 +24,7 @@ private:
 	ShvJournalEntry m_currentEntry;
 
 	const shv::chainpack::RpcValue m_log;
+	bool m_isThrowExceptions;
 	size_t m_currentIndex = 0;
 };
 
