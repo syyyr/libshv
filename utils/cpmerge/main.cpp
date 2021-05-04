3099aaaf0add5a75d8eb0c5f0492e928a9391b10
Jan Salus <salus@elektroline.cz>
2021-05-04 14:55:33 +0200

    rename site_config to overlay_config and useshv::chainpack::Utils::mergeMaps

--- utils/cpmerge/main.cpp
+++ utils/cpmerge/main.cpp
@@ -44,38 +44,18 @@ int main(int argc, char *argv[])
 		return 2;
 	}
 
-	shv::chainpack::RpcValue site_config = shv::chainpack::CponReader(std::cin).read(&err);
+	shv::chainpack::RpcValue overlay_config = shv::chainpack::CponReader(std::cin).read(&err);
 
 	if (err.length() > 0){
 		std::cerr << err << std::endl;
 		return 3;
 	}
 
-	shv::chainpack::RpcValue::Map merged_config = mergeRpcMap(site_config.toMap(), template_config.toMap());
-	shv::chainpack::RpcValue rv(merged_config);
+	shv::chainpack::RpcValue rv = shv::chainpack::Utils::mergeMaps(overlay_config.toMap(), template_config.toMap());
 
 	shv::chainpack::CponWriterOptions opts;
 	opts.setIndent("\t");
 	shv::chainpack::CponWriter wr(std::cout, opts);
 	wr << rv;
 	return 0;
-}
-
-
-shv::chainpack::RpcValue::Map mergeRpcMap(shv::chainpack::RpcValue::Map site_config, shv::chainpack::RpcValue::Map template_config)
-{
-	for(auto it : template_config.keys()) {
-		if(site_config.hasKey(it)) {
-			if(site_config.value(it).isMap() && template_config.value(it).isMap()) {
-				shv::chainpack::RpcValue::Map one(site_config.value(it).toMap());
-				shv::chainpack::RpcValue::Map two(template_config.value(it).toMap());
-				one = mergeRpcMap(one, two);
-				site_config[it] = one;
-			}
-		}
-		else {
-			site_config[it] = template_config.value(it);
-		}
-	}
-	return site_config;
 }
