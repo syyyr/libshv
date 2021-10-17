46af4fa9e190d0d390b7e4e809a2d03862836cf6
Fanda Vacek <fanda.vacek@gmail.com>
2021-10-17 18:18:36 +0200

    rpcvalue_cast<shv::chainpack::RpcValue::String> reimplemented using asString()

--- libshvchainpack/src/chainpack/rpcvalue.h
+++ libshvchainpack/src/chainpack/rpcvalue.h
@@ -531,6 +531,6 @@ template<> inline shv::chainpack::RpcValue rpcvalue_cast<shv::chainpack::RpcValu
 template<> inline bool rpcvalue_cast<bool>(const shv::chainpack::RpcValue &v) { return v.toBool(); }
 template<> inline shv::chainpack::RpcValue::Int rpcvalue_cast<shv::chainpack::RpcValue::Int>(const shv::chainpack::RpcValue &v) { return v.toInt(); }
 template<> inline shv::chainpack::RpcValue::UInt rpcvalue_cast<shv::chainpack::RpcValue::UInt>(const shv::chainpack::RpcValue &v) { return v.toUInt(); }
-template<> inline shv::chainpack::RpcValue::String rpcvalue_cast<shv::chainpack::RpcValue::String>(const shv::chainpack::RpcValue &v) { return v.toString(); }
+template<> inline shv::chainpack::RpcValue::String rpcvalue_cast<shv::chainpack::RpcValue::String>(const shv::chainpack::RpcValue &v) { return v.asString(); }
 template<> inline shv::chainpack::RpcValue::DateTime rpcvalue_cast<shv::chainpack::RpcValue::DateTime>(const shv::chainpack::RpcValue &v) { return v.toDateTime(); }
 template<> inline shv::chainpack::RpcValue::Decimal rpcvalue_cast<shv::chainpack::RpcValue::Decimal>(const shv::chainpack::RpcValue &v) { return v.toDecimal(); }
