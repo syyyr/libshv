0267e359325fbf8d1dc775cb4f1714720e6cb366
Václav Kubernát <sir.venceslas@gmail.com>
2022-10-18 11:06:22 +0200

    Fix ignoring Wuseless-cast on Clang

--- libshvbroker/src/rpc/masterbrokerconnection.cpp
+++ libshvbroker/src/rpc/masterbrokerconnection.cpp
@@ -171,10 +171,12 @@ void MasterBrokerConnection::onRpcDataReceived(shv::chainpack::Rpc::ProtocolType
 			else if (rq_id == m_masterBrokerIdRqId) {
 				m_masterBrokerIdRqId = 0;
 				cp::RpcValue rpc_val = decodeData(protocol_type, msg_data, 0);
+#if defined __GNUC__ && !defined(__clang__)
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wuseless-cast"
 				rpc_val.setMetaData(cp::RpcValue::MetaData(md));
 #pragma GCC diagnostic pop
+#endif
 
 				Q_EMIT masterBrokerIdReceived(cp::RpcResponse(cp::RpcMessage(rpc_val)));
 			}
