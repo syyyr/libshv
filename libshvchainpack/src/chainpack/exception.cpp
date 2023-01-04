3f5734f00f44a669b37dd3c276a41c2d95c37c02
Fanda Vacek <fvacek@elektroline.cz>
2023-01-04 20:53:35 +0100

    log every exception if abort-on-exception flag is set

--- libshvchainpack/src/chainpack/exception.cpp
+++ libshvchainpack/src/chainpack/exception.cpp
@@ -17,7 +17,7 @@ Exception::Exception(const std::string &_msg, const std::string &_where, const c
 	: m_msg(_msg)
 	, m_where(_where)
 {
-	if(!_where.empty() || (_log_topic && *_log_topic)) {
+	if(isAbortOnException() || !_where.empty() || (_log_topic && *_log_topic)) {
 		if(_log_topic && *_log_topic)
 			nCError(_log_topic) << "SHV_EXCEPTION:" << _where << _msg;
 		else
