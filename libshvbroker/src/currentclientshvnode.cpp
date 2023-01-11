6137a9b70db610b6f9c47d730d60f6d775bcbcf2
Václav Kubernát <sir.venceslas@gmail.com>
2023-01-11 11:27:56 +0100

    qt6: Fix deprecated QCryptographicHash::addData

--- libshvbroker/src/currentclientshvnode.cpp
+++ libshvbroker/src/currentclientshvnode.cpp
@@ -14,7 +14,11 @@ using namespace std;
 static string sha1_hex(const std::string &s)
 {
 	QCryptographicHash hash(QCryptographicHash::Algorithm::Sha1);
+#if QT_VERSION_MAJOR >= 6
+	hash.addData(QByteArrayView(s.data(), static_cast<int>(s.length())));
+#else
 	hash.addData(s.data(), static_cast<int>(s.length()));
+#endif
 	return std::string(hash.result().toHex().constData());
 }
 
