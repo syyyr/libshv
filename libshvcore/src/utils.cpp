d1660d8221d36a66dafeb9134816b71fdd041b53
Václav Kubernát <vkubernat@elektroline.cz>
2023-05-04 13:32:08 +0200

    fixup! libshvchainpack: Fix conversion warnings

--- libshvcore/src/utils.cpp
+++ libshvcore/src/utils.cpp
@@ -91,9 +91,9 @@ static inline char unhex_char(char c)
 	if(c >= '0' && c <= '9')
 		return c - '0';
 	if(c >= 'a' && c <= 'f')
-		return c - 'a' + 10;
+		return static_cast<char>(c - 'a' + 10);
 	if(c >= 'A' && c <= 'F')
-		return c - 'A' + 10;
+		return static_cast<char>(c - 'A' + 10);
 	return char(0);
 }
 
